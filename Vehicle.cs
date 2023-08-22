6457f399018e4baaba7738c16c45de244f1363d8
Carhcho <63563365+Carhcho@users.noreply.github.com>
2023-08-21 22:37:21 -0600

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dropbox03
+{
+    class Vehicle
+    {
+        //auto properties
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public char VehicleSymbol { get; set; }
+        public ConsoleColor VehicleColor { get; set; }
+        public int Speed { get; set; }
+        //constructor
+        public Vehicle(int xPosition, int yPosition, char vehicleSymbol, ConsoleColor vehicleColor, int speed)
+        {
+            XPosition = xPosition;
+            YPosition = yPosition;
+            VehicleSymbol = vehicleSymbol;
+            VehicleColor = vehicleColor;
+            Speed = speed;
+        }
+        //methods
+        public void MoveLeft()
+        {
+            XPosition--;
+        }
+        public void MoveRight()
+        {
+            XPosition ++;
+        }
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+        public void Brake()
+        {
+            Speed -= 10;
+        }
+    }
+}
